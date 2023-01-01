675de3d170ae14d26c2488582004d79332be0a32
ATixonov <tixxxonnnofff@rambler.ru>
2023-01-01 15:11:00 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2OOP/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Person Bob = new Person();
+//Bob.GetInfo();
+//Bob.name = "Bob";
+//Bob.age = 23;
+//Bob.GetInfo();
+//Person Tom = new Person();
+//Tom.name = "Tom";
+//Tom.age = 27;
+//Tom.GetInfo();
+//class Person
+//{
+//    public string name;
+//    public int age;
+//    public void GetInfo()
+//    {
+//        Console.WriteLine($"Name: {name}, Age: {age}");
+//    }
+//}
+
+//Person tom = new Person();
+//Person bob = new Person("Bob");
+//Person alice = new Person("Alice", 35);
+//Person oleg = new Person() {name = "Oleg", age = 49};
+//tom.GetInfo();
+//bob.GetInfo();
+//alice.GetInfo();
+//oleg.GetInfo();
+//class Person
+//{
+//    public string name;
+//    public int age;
+//    public Person()
+//    {
+//        name = "No name";
+//        age = 99;
+//    }
+//    public Person(string n)
+//    {
+//        name = n;
+//        age = 99;
+//    }
+//    public Person(string n, int a)
+//    {
+//        name = n;
+//        age = a;
+//    }
+//    public void GetInfo()
+//    {
+//        Console.WriteLine($"Name: {name}, Age: {age}");
+//    }
+//}
+
+//User tom;
+//tom.name = "Tom";
+//tom.age = 19;
+//tom.GetInfo();
+//User bob = new User();
+//bob.GetInfo();
+//User oleg = new User("Oleg", 25);
+//oleg.GetInfo();
+//User alice = new User("Oleg", 25) {name = "Alice", age = 35};
+//alice.GetInfo();
+//struct User
+//{
+//    public string name;
+//    public int age;
+//    public User(string n, int a)
+//    {
+//        name = n; age = a;
+//    }
+//    public void GetInfo()
+//    {
+//        Console.WriteLine($"Name: {name}, Age: {age}");
+//    }
+//}
